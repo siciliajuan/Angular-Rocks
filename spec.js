098c6818b75f4eb76747b697fe8d748a98de4411
Juan Sicilia <siciliajuan@gmail.com>
2015-11-12 10:23:40 +0100

    base test working

--- spec.js
+++ spec.js
@@ -1,8 +1,87 @@
 describe('Angular Rocks tests E2E', function() {
 
-  it('route tests', function() {
+  it('#/ tests', function() {
   	browser.get('http://localhost/Kubide/#/');
     expect(browser.getTitle()).toEqual('Angular Rocks');
+    /*
+    expect().toEqual('Esto es la descripción por defecto');
+    expect().toEqual('angular, rocks, default');
+    expect().toEqual('');
+    expect().toEqual('');
+    expect().toEqual('');
+
+    expect().toEqual('article');
+    */
   });
-  
+
+  it('#/foo tests', function() {
+  	browser.get('http://localhost/Kubide/#/foo');
+    expect(browser.getTitle()).toEqual('Angular Rocks');
+    /*
+    expect().toEqual('Esto es la descripción por defecto');
+    expect().toEqual('angular, rocks, default');
+    expect().toEqual('');
+    expect().toEqual('');
+    expect().toEqual('');
+
+    expect().toEqual('article');
+    */
+  });
+
+  it('#/bar tests', function() {
+  	browser.get('http://localhost/Kubide/#/bar');
+    expect(browser.getTitle()).toEqual('Angular Rocks');
+    /*
+    expect().toEqual('Esto es la descripción por defecto');
+    expect().toEqual('angular, rocks, default');
+    expect().toEqual('');
+    expect().toEqual('');
+    expect().toEqual('');
+
+    expect().toEqual('article');
+    */
+  });
+
+  it('#/the-rolling-stone tests', function() {
+  	browser.get('http://localhost/Kubide/#/the-rolling-stone');
+    expect(browser.getTitle()).toEqual('The Rolling Stone');
+    /*
+    expect().toEqual('Esto es la descripción por defecto');
+    expect().toEqual('angular, rocks, default');
+    expect().toEqual('');
+    expect().toEqual('');
+    expect().toEqual('');
+
+    expect().toEqual('article');
+    */
+  });
+
+  it('#/the-beatles tests', function() {
+  	browser.get('http://localhost/Kubide/#/the-beatles');
+    expect(browser.getTitle()).toEqual('The Beatles');
+    /*
+    expect().toEqual('Esto es la descripción por defecto');
+    expect().toEqual('angular, rocks, default');
+    expect().toEqual('');
+    expect().toEqual('');
+    expect().toEqual('');
+
+    expect().toEqual('article');
+    */
+  });
+
+  it('#/queen tests', function() {
+  	browser.get('http://localhost/Kubide/#/queen');
+    expect(browser.getTitle()).toEqual('Queen');
+    /*
+    expect().toEqual('Esto es la descripción por defecto');
+    expect().toEqual('angular, rocks, default');
+    expect().toEqual('');
+    expect().toEqual('');
+    expect().toEqual('');
+
+    expect().toEqual('article');
+    */
+  });
+
 });
